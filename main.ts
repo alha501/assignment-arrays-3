e8625972fb7a8120f6be4773bad1d72ee93881d3
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-08 09:50:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,15 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("" + (text_list.length))
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index <= 5; index++) {
-        basic.showString("" + (text_list.removeAt(randint(0, 5))))
+    for (let index = 0; index < 5; index++) {
+        test = text_list.removeAt(randint(0, 5))
+        basic.showString("" + (test))
     }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (text_list.removeAt(randint(0, 5))))
 })
+let test = ""
 let text_list: string[] = []
 text_list = [
 "Thanos",
